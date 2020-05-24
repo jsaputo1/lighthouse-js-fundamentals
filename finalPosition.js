999c2e736a5a5aa1273060b64154ced1afcbea50
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-05-24 14:30:18 -0400

    updated solution

--- finalPosition.js
+++ finalPosition.js
@@ -1,27 +1,21 @@
-const moves = [
-  ["north"],
-  ["north"],
-  ["west"],
-  ["west"],
-  ["north"],
-  ["east"],
-  ["north"],
-];
-
+const moves = ["north", "north", "west", "west", "north", "east", "north"];
 function finalPosition(moves) {
-  let position = [0, 0];
-
-  for (let i = 0; i < 4; i++) {
-    if (moves === "north") {
-      position[1] + 1;
-    } else if (moves === "south") {
-      position[1] - 1;
-    } else if (moves === "east") {
-      position[0] + 1;
-    } else if (moves === "west") {
-      position[0] - 1;
+  let finalCoordinates = [];
+  let x = 0;
+  let y = 0;
+  for (const direction of moves) {
+    if (direction === "north") {
+      y += 1;
+    } else if (direction === "south") {
+      y -= 1;
+    } else if (direction === "east") {
+      x += 1;
+    } else if (direction === "west") {
+      x -= 1;
     }
   }
-  return position;
+  finalCoordinates.push(x);
+  finalCoordinates.push(y);
+  return finalCoordinates;
 }
-console.log(finalPosition());
+console.log(finalPosition(moves));
