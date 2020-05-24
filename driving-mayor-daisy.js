3b37937923edae8ec439f76dddc6325f730c0bad
John Saputo <jsaputo1@gmail.com>
2020-05-24 19:40:58 +0000

    updated code

--- driving-mayor-daisy.js
+++ driving-mayor-daisy.js
@@ -1,3 +1,15 @@
+const carPassing(cars, speed) {
+  const time = Date.now();
+
+  const car = {
+    time: time,
+    speed: speed,
+  };
+
+  cars.push(car);
+  return cars;
+}
+
 const cars = [
   {
     time: 1568329654807,
@@ -15,23 +27,4 @@ const cars = [
 
 const speed = 38;
 
-function carPassing(cars, speed) {
-  [
-    {
-      time: 1568329654807,
-      speed: 40,
-    },
-    {
-      time: 1568329821632,
-      speed: 42,
-    },
-    {
-      time: 1568331115463,
-      speed: 35,
-    },
-    {
-      time: 1568431216417,
-      speed: 38,
-    },
-  ];
-}
+console.log(carPassing(cars, speed));
